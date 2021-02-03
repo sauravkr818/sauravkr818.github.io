295b29a7fdd203985e6e0e5a48d1377e4a2161e4
sauravkr818 <sauravkr818@gmail.com>
2021-02-03 20:48:27 +0530

    [ADD]Some more changes in navbar

--- index.js
+++ index.js
@@ -22,3 +22,23 @@ $("h1").hover(
     });
   }
 );
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
+        datasets: [{
+            label: 'My Github Commits',
+            backgroundColor: '#e4fff4',
+            borderColor: 'black',
+            data: [0, 10, 5, 2, 20, 30, 45,10,10,1,110,10,11]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});

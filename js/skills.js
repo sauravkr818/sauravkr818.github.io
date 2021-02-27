ab74bfbb6b8a9a732d24e4a1b9e7cdb314ec762d
sauravkr818 <sauravkr818@gmail.com>
2021-02-27 23:10:10 +0530

    [UPDATE]added new js files
    This adds and updates js files

---
+++ js/skills.js
@@ -0,0 +1,95 @@
+let skills = `    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4  mb-5" style="margin-top:0px;" id="2">
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded slide-right">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-html5 fa-3x" style="color: rgb(240, 101, 41);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">HTML</h5>
+
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-css3-alt fa-3x" style="color: rgb(38, 77, 228);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">CSS</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-js fa-3x" style="color: rgb(250, 224, 53);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">JavaScript</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-react fa-3x fa-spin" style="color: rgb(99, 148, 238);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">ReactJs</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-python fa-3x" style="color: rgb(99, 148, 238);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">Python</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col pt-5 d-flex justify-content-center">
+    <div class="card h-100 shadow animate w-75 border-0 rounded">
+        <div class="card-header bg-white border-0 mx-auto">
+            <i class="fab fa-cuttlefish fa-3x" style="color: rgb(99, 148, 238);"></i>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center text-primary">C</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="progress w-75 ms-4" style="height:6px;">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-secondary" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>`
+
+$("#skills").html(skills);
---
+++ js/topShow.js
@@ -0,0 +1,28 @@
+let topShow = `<div class="row" style="padding-top: 200px" id="home">
+<div class="col-sm-12 col-md-8 d-flex justify-content-center justify-content-lg-start">
+    <div class="ms-lg-5">
+        <p class="text-dark text-center text-lg-start">Hello World, I'm</p>
+        <h1 class="abc text-center text-lg-start" style="
+    font-weight: 900;
+    line-height: 90%;
+    color: rgb(61, 61, 61);
+    font-size: 2.2rem;
+  ">
+            SAURAV&nbsp;KUMAR
+        </h1>
+        <h6 class="abc text-center text-lg-start" style="font-size: 1.06rem; line-height: 150%; letter-spacing: 2px">
+            FULL STACK WEB DEVELOPER
+        </h6>
+    </div>
+</div>
+<div class="col-sm-12 col-md-4 d-flex d-lg-flex-0 justify-content-center justify-content-lg-start">
+    <div class="card w-75 shadow border-0 rounded animate slide-left mt-4 mt-lg-0">
+        <div class="card-body text-primary text-center text-lg-start">
+            <p class="transition">1. The most important part of life is <b>Failures</b></p>
+            <p>2. And ofcourse your <b>Family</b></p>
+        </div>
+    </div>
+</div>
+</div>`
+
+$("#top-show").html(topShow);

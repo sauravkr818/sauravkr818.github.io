20da4ea5d259900ac676358d440bf44427962b61
sauravkr818 <sauravkr818@gmail.com>
2021-05-12 16:09:02 +0530

    [FIX]UI

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,10 @@
 let footer = `<footer className="mb-0 py-4">
 
-<div class="row justify-content-center py-5 alert-primary" style="background-color: #606dff;">
+<div class="row justify-content-center py-5 alert-success">
     <div class="col-12">
         <div class="row">
             <div class="col-md-2 col-sm-2 my-auto mx-auto ">
-                <h3 class="mb-md-0 mb-5 font-weight-bold text-white">Saurav Kumar</h3>
+                <h3 class="mb-md-0 mb-5 font-weight-bold">Saurav Kumar</h3>
                 <div class=" d-sm-flex justify-content-start d-block pt-lg-3">
                     <a href="https://github.com/sauravkr818"><i class="fab fa-github fa-2x pe-lg-3 pe-2 hover-github"></i></a>
                     <a href="javascript:void(0)"><i class="fab fa-facebook fa-2x pe-lg-3 pe-2 hover-fb"></i></a>
@@ -14,21 +14,21 @@ let footer = `<footer className="mb-0 py-4">
             </div>
 
             <div class=" col-md-2 col-sm-2">
-                <h6 class="mb-3 mb-lg-4 bold-text text-white pt-3 pt-sm-0"><b>MENU</b></h6>
+                <h6 class="mb-3 mb-lg-4 bold-text pt-3 pt-sm-0"><b>MENU</b></h6>
                 <ul class="list-unstyled">
-                    <li><a href="" class="text-white">Home</a></li>
-                    <li class="text-white"><a href="" class="text-white">About</a></li>
-                    <li><a href="" class="text-white">Blog</a></li>
-                    <li><a href="" class="text-white">Portfolio</a></li>
+                    <li><a href="" class="text-success">Home</a></li>
+                    <li class="text-success"><a href="" class="text-success">About</a></li>
+                    <li><a href="" class="text-success">Blog</a></li>
+                    <li><a href="" class="text-success">Portfolio</a></li>
                 </ul>
             </div>
             <div class=" col-md-2 col-sm-2">
-                <h6 class="mb-3 mb-lg-4 bold-text  text-white"><b>MENU</b></h6>
+                <h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
                 <ul class="list-unstyled">
-                    <li><a href="" class="text-white">Home</a></li>
-                    <li><a href="" class="text-white">About</a></li>
-                    <li><a href="" class="text-white">Blog</a></li>
-                    <li><a href="" class="text-white">Portfolio</a></li>
+                    <li><a href="" class="text-success">Home</a></li>
+                    <li><a href="" class="text-success">About</a></li>
+                    <li><a href="" class="text-success">Blog</a></li>
+                    <li><a href="" class="text-success">Portfolio</a></li>
                 </ul>
             </div>
             <div class=" col-md-2 col-sm-2">
@@ -42,17 +42,13 @@ let footer = `<footer className="mb-0 py-4">
 </div>
 </footer>
 <footer class="">
-
 <div class="row row-1 pb-3">
     <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
         <small class="small"><span>&#174;</span>No copy rights | Feel free to copy</small>
     </div>
     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
         <h6 class="mt-55 mt-2 text-muted bold-text"><b>SAURAV KUMAR</b></h6>
-        <a href="mailto:sauravkr818@gmail.com"> <small> <span><i class="fa fa-envelope text-hover" aria-hidden="true"></i></span> sauravkr818@gmail.com</small></a>
-    </div>
-    <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
-        <h6 class="text-muted bold-text"><b>Saurav Kumar</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> helloWorld@gmail.com</small>
+        <a href="mailto:sauravkr818@gmail.com"> <small> <span><i class="fa fa-envelope text-hover pe-2" aria-hidden="true"></i></span>business.sauravkr818@gmail.com</small></a>
     </div>
 </div>
 
